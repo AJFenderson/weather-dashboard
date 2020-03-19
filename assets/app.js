125799f56b04e5526fa923a5137115eecbec2143
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-03-18 20:01:39 -0500

    button works

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+//create momentjs
+//grab a key from api weather
+// create the local storage
+// create a function to keep the records of the city
+//create a function to run the time
+//create a function that will run the colors of the uv
+// create a function that will run the weather
+//create a function that will fun the 5 day forecast
+// need 3 ajax calls
+// 1. 5 days of weather 2.for the uv 3. current date
+// make my button work
+$("#btn").on("click" ,function(e){
+e.preventDefault();
+console.log("click me");
+
+
+
+
+
+
+
+
+
+
+});

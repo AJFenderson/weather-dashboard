2b513c776390e869bab846d9b1529981bf31e3a9
Andrea Fenderson <andrea_fenderson@ymail.com>
2020-03-18 20:34:54 -0500

    created date and time using moment js

--- assets/app.js
+++ assets/app.js
@@ -1,11 +1,17 @@
-
+var todaysDate= $("#time").text(setTime);
 //grab a key from api weather
 var key= "71b2fba15d028a1cd9dcf3e77b5127d0";
 var queryUrl= "api.openweathermap.org/data/2.5/forecast?q=Nashville&appid=" + key;
 console.log(queryUrl);
+
+//create a function to run the time
+function setTime(){
+    var time= moment().format('MMMM Do YYYY, h:mm:ss a');
+    return time;
+
+}
 // create the local storage
 // create a function to keep the records of the city
-//create a function to run the time
 //create a function that will run the colors of the uv
 // create a function that will run the weather
 //create a function that will fun the 5 day forecast
